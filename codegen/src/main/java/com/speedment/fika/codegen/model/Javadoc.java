55b7c463d7802b3536961acad345c65440dcf697
Emil Forslund <emil.forslund@pyknic.se>
2016-05-07 14:50:06 -0700

    Upload CodeGen version 2.4.0-SNAPSHOT

---
+++ codegen/src/main/java/com/speedment/fika/codegen/model/Javadoc.java
@@ -0,0 +1,71 @@
+/**
+ *
+ * Copyright (c) 2006-2016, Speedment, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); You may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.speedment.fika.codegen.model;
+
+import com.speedment.fika.codegen.internal.model.JavadocImpl;
+import com.speedment.fika.codegen.model.trait.HasCall;
+import com.speedment.fika.codegen.model.trait.HasCopy;
+import com.speedment.fika.codegen.model.trait.HasJavadocTags;
+
+/**
+ * A model that represents a block of documentation in code. 
+ * 
+ * @author  Emil Forslund
+ * @see     JavadocTag
+ * @since  2.0
+ */
+public interface Javadoc extends HasCopy<Javadoc>, HasCall<Javadoc>, 
+    HasJavadocTags<Javadoc> {
+    
+    /**
+     * Sets the body text shown in the javadoc.
+     * 
+     * @param text  the text
+     * @return      a reference to this model
+     */
+    Javadoc setText(String text);
+
+    /**
+     * Returns the body text shown in the javadoc.
+     * 
+     * @return  the body documentation text
+     */
+    String getText();
+
+    /**
+     * Creates a new instance implementing this interface by using the class
+     * supplied by the default factory. To change implementation, please use
+     * the {@link #setSupplier(java.util.function.Supplier) setSupplier} method.
+
+     * @return      the new instance
+     */
+    static Javadoc of() {
+        return new JavadocImpl();
+    }
+    
+    /**
+     * Creates a new instance implementing this interface by using the class
+     * supplied by the default factory. To change implementation, please use
+     * the {@link #setSupplier(java.util.function.Supplier) setSupplier} method.
+     * 
+     * @param text  the documentation
+     * @return      the new instance
+     */
+    static Javadoc of(String text) {
+        return of().setText(text);
+    }
+}
---
+++ codegen/src/main/java/com/speedment/fika/codegen/model/Method.java
@@ -0,0 +1,54 @@
+/**
+ *
+ * Copyright (c) 2006-2016, Speedment, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); You may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.speedment.fika.codegen.model;
+
+import com.speedment.fika.codegen.internal.model.MethodImpl;
+import com.speedment.fika.codegen.model.modifier.MethodModifier;
+import com.speedment.fika.codegen.model.trait.HasAnnotationUsage;
+import com.speedment.fika.codegen.model.trait.HasCall;
+import com.speedment.fika.codegen.model.trait.HasCode;
+import com.speedment.fika.codegen.model.trait.HasCopy;
+import com.speedment.fika.codegen.model.trait.HasFields;
+import com.speedment.fika.codegen.model.trait.HasGenerics;
+import com.speedment.fika.codegen.model.trait.HasJavadoc;
+import com.speedment.fika.codegen.model.trait.HasName;
+import com.speedment.fika.codegen.model.trait.HasThrows;
+import com.speedment.fika.codegen.model.trait.HasType;
+
+/**
+ * A model that represents a method declaration in code.
+ *
+ * @author Emil Forslund
+ * @since  2.0
+ */
+public interface Method extends HasName<Method>, HasType<Method>, HasThrows<Method>,
+    HasGenerics<Method>, HasFields<Method>, HasJavadoc<Method>, HasAnnotationUsage<Method>,
+    HasCode<Method>, HasCall<Method>, MethodModifier<Method>, HasCopy<Method> {
+
+    /**
+     * Creates a new instance implementing this interface by using the class
+     * supplied by the default factory. To change implementation, please use the
+     * {@link #setMapper(java.util.function.BiFunction) setSupplier} method.
+     *
+     * @param name the name
+     * @param type the type
+     * @return the new instance
+     */
+    static Method of(String name, Type type) {
+        return new MethodImpl(name, type);
+    }
+}
